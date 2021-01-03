4f7c087aaeced07dad0aacddf6229ea96a394f9f
João Vitor de Faria <jvfaria015@gmail.com>
2021-01-03 16:04:02 -0300

    structure completed

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 
 import { FiLogIn, FiLock, FiMail } from 'react-icons/fi';
-
+import * as Yup from 'yup';
 import { Form } from '@unform/web';
 import { Link } from 'react-router-dom';
+import { FormHandles } from '@unform/core';
+import getValidationErrors from '../../utils/getValidationErrors';
 import { Container, Content, Background } from './styles';
 import Input from '../../components/input';
 
@@ -12,20 +14,40 @@ import Button from '../../components/button';
 import logo from '../../assets/logo.svg';
 
 const Login: React.FC = () => {
-  function handleSubmit(data: string): void {
-    console.log(data);
-  }
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Email obrigatório')
+          .email('Digite um email válido'),
+        password: Yup.string()
+          .required('Senha obrigatória'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      const errors = getValidationErrors(error);
+      formRef.current?.setErrors(
+        errors,
+      );
+    }
+  }, []);
   return (
     <Container>
       <Content>
         <img src={logo} alt="" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu login</h1>
 
           <Input icon={FiMail} name="email" placeholder="E-mail" />
 
-          <Input type="password" icon={FiLock} name="senha" placeholder="Password" />
+          <Input type="password" icon={FiLock} name="password" placeholder="Password" />
 
           <Button type="submit">Entrar</Button>
 
--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,33 +1,58 @@
-import React from 'react';
-
+import React, { useCallback, useRef } from 'react';
+import { FormHandles } from '@unform/core';
 import {
   FiArrowLeft, FiLock, FiMail, FiUser,
 } from 'react-icons/fi';
-
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import { Link } from 'react-router-dom';
 import { Container, Content, Background } from './styles';
 import Input from '../../components/input';
 import Button from '../../components/button';
 import logo from '../../assets/logo.svg';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 const Signup: React.FC = () => {
-  function handleSubmit(data: string): void {
-    console.log(data);
-  }
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        name: Yup
+          .string()
+          .required('Nome obrigatório'),
+        email: Yup
+          .string()
+          .email('Digite um email válido')
+          .required('Email obrigatório'),
+        password: Yup
+          .string()
+          .min(6, 'Mínimo 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      const errors = getValidationErrors(error);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={logo} alt="" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit} ref={formRef}>
 
           <Input icon={FiUser} name="name" placeholder="Nome" />
 
           <Input icon={FiMail} name="email" placeholder="E-mail" />
 
-          <Input type="password" icon={FiLock} name="senha" placeholder="Password" />
+          <Input type="password" icon={FiLock} name="password" placeholder="Password" />
 
           <Button type="submit">Cadastrar</Button>
 
