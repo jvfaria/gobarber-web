f60688d9acdf021216e168d3a074441d6a08b194
João Vitor de Faria <jvfaria015@gmail.com>
2021-01-05 20:59:47 -0300

    auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 import Routes from './routes/index.routes';
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <Routes />
+    <AuthProvider>
+      <Routes />
+
+    </AuthProvider>
     <GlobalStyle />
   </>
 );
