217837c4425855699a6e9c6e67bbcec8ff614c5e
João Vitor de Faria <jvfaria015@gmail.com>
2021-01-10 22:00:58 -0300

    login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef, useContext } from 'react';
+import React, { useCallback, useRef } from 'react';
 
 import { FiLogIn, FiLock, FiMail } from 'react-icons/fi';
 import * as Yup from 'yup';
@@ -8,7 +8,9 @@ import { FormHandles } from '@unform/core';
 import getValidationErrors from '../../utils/getValidationErrors';
 import { Container, Content, Background } from './styles';
 import Input from '../../components/input';
-import { AuthContext } from '../../context/AuthContext';
+
+import { useToast } from '../../hooks/toast';
+import { useAuth } from '../../hooks/auth';
 import Button from '../../components/button';
 
 import logo from '../../assets/logo.svg';
@@ -21,7 +23,8 @@ interface DataProps {
 const Login: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { login } = useContext(AuthContext);
+  const { login } = useAuth();
+  const { addToast } = useToast();
 
   const handleSubmit = useCallback(async (data: DataProps) => {
     try {
@@ -34,20 +37,32 @@ const Login: React.FC = () => {
           .required('Senha obrigatória'),
       });
 
-      login({
+      await login({
         email: data.email,
         password: data.password,
       });
       await schema.validate(data, {
         abortEarly: false,
       });
+      addToast({
+        type: 'success',
+        title: 'Login efetuado com sucesso !',
+      });
     } catch (error) {
-      const errors = getValidationErrors(error);
-      formRef.current?.setErrors(
-        errors,
-      );
+      if (error instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(error);
+        formRef.current?.setErrors(
+          errors,
+        );
+      }
+
+      addToast({
+        type: 'error',
+        title: 'Erro na autenticação',
+        description: 'Ocorreu um erro ao fazer login, cheque as credenciais',
+      });
     }
-  }, [login]);
+  }, [login, addToast]);
   return (
     <Container>
       <Content>
